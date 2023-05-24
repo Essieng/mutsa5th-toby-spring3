257d2179f838b1fd3b69d9cffa9e17b7365c7230
Jason Park <qwt0304@gmail.com>
2023-05-24 13:54:36 +0900

    템플릿 메소드 패턴 적용

---
+++ src/main/java/com/example/tobyspring3/dao/DUserDao.java
@@ -0,0 +1,17 @@
+package com.example.tobyspring3.dao;
+
+import java.sql.*;
+import java.util.Map;
+
+import static java.lang.System.getenv;
+
+public class DUserDao extends UserDao{
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                "", "", ""
+        );
+        return conn;
+    }
+}
--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -7,20 +7,22 @@ import java.util.Map;
 
 import static java.lang.System.getenv;
 
-public class UserDao {
+public abstract class UserDao {
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Map<String, String> env = getenv();
-        String dbHost = env.get("DB_HOST"); //DB_HOST=jdbc:mysql://localhost:3306/spring-db
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(
-                dbHost, dbUser, dbPassword
-        );
-        return conn;
-    }
+//    {
+//        Map<String, String> env = getenv();
+//        String dbHost = env.get("DB_HOST"); //DB_HOST=jdbc:mysql://localhost:3306/spring-db
+//        String dbUser = env.get("DB_USER");
+//        String dbPassword = env.get("DB_PASSWORD");
+//
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection conn = DriverManager.getConnection(
+//                dbHost, dbUser, dbPassword
+//        );
+//        return conn;
+//    }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         Map<String, String> env = getenv();
@@ -68,10 +70,10 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
-        User selectedUser = userDao.get("3");
-        System.out.println(selectedUser.getId());
-        System.out.println(selectedUser.getName());
-        System.out.println(selectedUser.getPassword());
+//        UserDao userDao = new UserDao();
+//        User selectedUser = userDao.get("3");
+//        System.out.println(selectedUser.getId());
+//        System.out.println(selectedUser.getName());
+//        System.out.println(selectedUser.getPassword());
     }
 }
